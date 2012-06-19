0375ded595f2a49a69b4ca4aa2375f82509a16f5
Niclas Olofsson <n@niclasolofsson.se>
2012-06-19 23:09:53 +0200

    Require login for main view

--- entries/views.py
+++ entries/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 from entries.models import LifeEntry
 
 
+@login_required
 def index(request):
     e = LifeEntry.get_entries()
     return render(request, 'index.html', {"entries": e})
