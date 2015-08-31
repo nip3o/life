665fc3baa1aa126f37b5685653392f7e51edffbc
Niclas Olofsson <n@niclasolofsson.se>
2015-08-31 22:56:56 +0200

    Proper static and media path settings.

--- life/settings.py
+++ life/settings.py
@@ -10,9 +10,6 @@ TEMPLATE_DEBUG = DEBUG
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.abspath(os.path.join(PROJECT_DIR, '..')))
 
-
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
-
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -51,18 +48,18 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_DIR, '../media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, '../static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
